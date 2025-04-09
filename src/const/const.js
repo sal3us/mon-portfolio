2813d48dadae5053e36e137bc093f1a176ae253f
salah lhassibi <salah.lhassibi@gmail.com>
2025-04-09 09:52:30 +0100

    Initial commit

---
+++ src/const/const.js
@@ -0,0 +1,92 @@
+export const studies = [
+  {
+    title: "Técnico en desarrollo de software",
+    corporation: "SENA - Montería (Córdoba) ",
+    date: "02/2019 – 12/2019",
+  },
+  {
+    title: "Desarrollador Front-end React Js",
+    corporation: "Alura Latam - Remoto",
+    date: "04/2023 – 09/2023",
+  },
+  {
+    title: "Curso profesional de React Js",
+    corporation: "Código facilito - Remoto",
+    date: "04/2023 – 04/2023",
+  },
+];
+
+export const IconKeys = {
+  html: "html",
+  css: "css",
+  javaScript: "javaScript",
+  react: "react",
+  astro: "astro",
+  materialUi: "materialUi",
+  nextjs: "nextjs",
+  redux: "redux",
+  styled: "styled",
+  tailwind: "tailwind",
+  radix: "radix",
+  reactQuery: "reactQuery",
+  typeScript: "typeScript",
+  zustand: "zustand",
+  git: "git",
+  github: "github",
+  stripe: "stripe",
+  shadcn: "shadcn",
+  nodejs: "nodejs",
+  mongodb: "mongodb",
+  vite: "vite",
+  // ici, ajoutez d'autres clés d'icônes si nécessaire
+};
+export const skills = [
+  { title: "HTML", icon: "html" },
+  { title: "CSS", icon: "css" },
+  { title: "JavaScript", icon: "javaScript" },
+  { title: "TypeScript", icon: "typeScript" },
+  { title: "React.js", icon: "react" },
+  { title: "Next.js", icon: "nextjs" },
+  { title: "Tailwind CSS", icon: "tailwind" },
+  { title: "Redux.js", icon: "redux" },
+  { title: "Node.js", icon: "nodejs" },
+  { title: "Laravel", icon: "laravel" },
+  { title: "MongoDB", icon: "mongodb" },
+  { title: "Git & GitHub", icon: "git" },
+  { title: "WordPress", icon: "wordpress" },
+  { title: "SQL", icon: "sql" }, // Ajout de SQL
+];
+
+
+export const projects = [
+  {
+    title: "Rédaction Raffinée",
+    img: "/img/redaction.jpeg",
+    description: "Site de rédaction d’articles proposant un contenu riche et une plume raffinée.",
+    link: "https://xn--rdactionraffine-bnbn.com/"
+  },
+  {
+    title: "seotechservice",
+    img: "/img/seo-tech.jpeg",
+    description: "Site vitrine présentant les services SEO de manière efficace.",
+    link: "https://seotechservice.net/"
+  },
+  {
+    title: "top5review-tech",
+    img: "/img/Blog.jpeg",
+    description: "Blog de technologie proposant des avis et analyses sur les dernières tendances tech.",
+    link: "https://top5review-tech.com/"
+  },
+  {
+    title: "ecrinduregard",
+    img: "/img/ecran.jpeg",
+    description: "Site d’opticien offrant une expérience de vision claire et une interface soignée.",
+    link: "https://ecrinduregard.com/"
+  },
+  {
+    title: "rankfirst-agency",
+    img: "/img/Rank.jpeg",
+    description: "Site informatif présentant l'agence et ses partenariats majeurs pour une expérience publicitaire inégalée.",
+    link: "https://rankfirst-agency.com/"
+  }
+];
