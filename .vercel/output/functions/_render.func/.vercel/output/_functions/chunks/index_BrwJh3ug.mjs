2adcd952cdede9841df6fc5dc492aaf1d739923b
salah lhassibi <salah.lhassibi@gmail.com>
2025-04-09 11:58:58 +0100

    Fix: Modify Astro config for static output and Vercel runtime

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_BrwJh3ug.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/index_BdN9kYZY.mjs');
+
+export { page };
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 
-// Configuration d'Astro avec Vercel en utilisant un runtime correct et la sortie statique
 export default defineConfig({
-  output: "static",  // Si tu ne veux pas utiliser de fonctions serverless, "static" est souvent plus simple.
+  output: "server", // Assurez-vous que cette ligne est présente
   adapter: vercel({
     webAnalytics: {
-      enabled: true,  // Tu peux désactiver ceci si tu n'as pas besoin de la fonctionnalité Web Analytics
-}}),
+      enabled: true,
+    },
+  }),
 });
